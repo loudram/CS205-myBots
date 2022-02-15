c52c52230e437dbec66f61786d148da683228329
loudram <sgusick@uvm.edu>
2022-02-14 20:06:19 -0500

    Final motors push

--- simulate.py
+++ simulate.py
@@ -3,6 +3,16 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
+import math
+import random as random
+
+pi = math.pi
+amplitudeBackLeg = pi/4
+frequencyBackLeg = 10
+phaseOffsetBackLeg = 0
+amplitudeFrontLeg = pi/4
+frequencyFrontLeg = 15
+phaseOffsetFrontLeg = pi/4
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -10,7 +20,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.setGravity(0,0,-9.8)
-p.loadSDF("world.sdf")
+#p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
@@ -18,6 +28,19 @@ programRunTime = 1000
 
 backLegSensorValues = numpy.zeros(programRunTime)
 frontLegSensorValues = numpy.zeros(programRunTime)
+
+val = frequencyBackLeg * numpy.linspace(0, 2 * pi, programRunTime) + phaseOffsetBackLeg
+targetAnglesBackLeg = amplitudeBackLeg * numpy.sin(val)
+
+val2 = frequencyFrontLeg * numpy.linspace(0, 2 * pi, programRunTime) + phaseOffsetFrontLeg
+targetAnglesFrontLeg = amplitudeFrontLeg * numpy.sin(val2)
+#numpy.save('data\\targetAngles.npy', targetAngles)
+
+
+
+
+i = 0
+
 
 for x in range(programRunTime):
     p.stepSimulation()
@@ -29,10 +52,16 @@ for x in range(programRunTime):
     pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, 
                                 jointName = "Torso_BackLeg", 
                                 controlMode = p.POSITION_CONTROL, 
-                                targetPosition = 0.0, 
-                                maxForce = 0.0)
-
-    time.sleep(1/60)
+                                targetPosition =  targetAnglesBackLeg[i], 
+                                maxForce = 100)
+    
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, 
+                                jointName = "Torso_FrontLeg", 
+                                controlMode = p.POSITION_CONTROL, 
+                                targetPosition =  targetAnglesFrontLeg[i], 
+                                maxForce = 100)
+    i += 1
+    time.sleep(1/240)
 
 print(backLegSensorValues)
 numpy.save('data\\backLegSensorValues.npy', backLegSensorValues)
